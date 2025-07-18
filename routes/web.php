32b194549e6ab9d2ecfe0e7f76edba9fab5854d7
Max Kunz <maxxkunz@gmail.com>
2025-07-18 16:17:44 +0200

    Neuen Menüpunkt hinzugefügt

--- routes/web.php
+++ routes/web.php
@@ -11,5 +11,9 @@ Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('performancemonitor', function () {
+    return Inertia::render('PerformanceMonitor');
+})->middleware(['auth', 'verified'])->name('performancemonitor');
+
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
