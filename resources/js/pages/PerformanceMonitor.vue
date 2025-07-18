5d7bd592c6a79ccd26a609420cc2a92dba0aeb7e
Max Kunz <maxxkunz@gmail.com>
2025-07-18 16:50:11 +0200

    Headline für Page hinzugefügt

--- resources/js/pages/PerformanceMonitor.vue
+++ resources/js/pages/PerformanceMonitor.vue
@@ -3,6 +3,7 @@ import AppLayout from '@/layouts/AppLayout.vue';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/vue3';
 import PlaceholderPattern from '../components/PlaceholderPattern.vue';
+import Heading from '@/components/Heading.vue';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -17,6 +18,7 @@ const breadcrumbs: BreadcrumbItem[] = [
 
     <AppLayout :breadcrumbs="breadcrumbs">
         <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4 overflow-x-auto">
+            <Heading title="Performance-Monitor" description="Interviewer-Performance live" />
             <div class="grid auto-rows-min gap-4 md:grid-cols-3">
                 <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
                     <PlaceholderPattern />
